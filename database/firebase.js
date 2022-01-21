db96d3f8a27a20a7dd86eb8bc6410c97ba03900a
azri hasin <azrihasin1999@gmail.com>
2022-01-21 14:53:31 +0800

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -4,14 +4,6 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage'
 
 // Initialize Firebase
-const firebaseConfig = {
-    apiKey: 'AIzaSyAXhvpTNG-j6cjYat3qX42IxwqgQLDiOsc',
-    authDomain: 'reactnativefirebase-9ddfe.firebaseapp.com',
-    projectId: 'reactnativefirebase-9ddfe',
-    storageBucket: 'reactnativefirebase-9ddfe.appspot.com',
-    messagingSenderId: '830052257834',
-    appId: '1:830052257834:web:d5edb506b4eda352b73353',
-};
 
 //REPLACE THIS FIREBASE CONFIG WITH YOUR FIREBASE CONFIG
 
